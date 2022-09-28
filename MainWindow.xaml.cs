81ddd5145f63e74fac889278bab9706fa6190a10
Shane Morrison <shanemor@gmail.com>
2022-09-28 15:50:38 -0400

    from script

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Commons.Music.Midi;
+using CoreMidi;
+using MeltySynth;
+using NAudio.Wave;
+
+namespace midiplayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WaveOut waveOut;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var player = new MidiSampleProvider("TimGM6mb.sf2");
+
+            waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            waveOut.Init(player);
+            waveOut.Play();
+
+            // Load the MIDI file.
+            var midiFile = new MeltySynth.MidiFile(@"C:\Users\shane\Downloads\Never-Gonna-Give-You-Up-3.mid");
+
+            // Play the MIDI file.
+            player.Play(midiFile, true);            
+        }
+    }
+}
