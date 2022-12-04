128ef92bf583918952ef7ba702562ec9c6b7b84c
Shane Morrison <shanemor@gmail.com>
2022-12-03 23:11:18 -0500

    from script

---
+++ midiprocess/Program.cs
@@ -0,0 +1,21 @@
+﻿using midilib;
+using System.Diagnostics;
+
+namespace midiprocess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process().Wait();
+        }
+
+        static async Task<bool> Process()
+        {
+            MidiDb db = new MidiDb();
+            await db.Initialize();
+            SongDb sdb = new SongDb(db);
+            return await sdb.Build();
+        }
+}
+}
