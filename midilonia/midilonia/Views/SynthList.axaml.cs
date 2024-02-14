842ef1cfc535dd1c523c563b9c0ff347a402cfba
Shane Morrison <shanemor@gmail.com>
2024-02-14 17:26:28 -0500

    up

---
+++ midilonia/midilonia/Views/SynthList.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace midilonia.Views
+{
+    public partial class SynthList : UserControl
+    {
+        public SynthList()
+        {
+            InitializeComponent();
+        }
+    }
+}
