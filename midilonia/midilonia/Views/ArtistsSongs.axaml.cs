dd83bb1fd30752154fb2898f8a60dac4f04f9278
Shane Morrison <shanemor@gmail.com>
2024-02-16 15:57:50 -0500

    up

---
+++ midilonia/midilonia/Views/ArtistsSongs.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using midilib;
+using NAudio.Midi;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using static midilib.MidiDb;
+
+namespace midilonia.Views;
+
+public partial class ArtistsSongs : UserControl
+{
+    public ArtistsSongs()
+    {
+        DataContext = App.ViewModel;
+        InitializeComponent();
+    }
+      
+}
